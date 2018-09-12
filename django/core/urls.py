d4c01535592f65ffffbe23557e24d2cef278b763
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-12 17:14:50 +1000

    added urls for new views

--- django/core/urls.py
+++ django/core/urls.py
@@ -5,7 +5,15 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('answer/<int:pk>',
-         views.AnswerDetailView.as_view(), name='answer-detail'),
+         views.AnswerDetailView.as_view(),
+         name='answer-detail'),
     path('ask/<int:pk>',
-         views.CreateQuestionView.as_view(), name='ask'),
+         views.CreateQuestionView.as_view(),
+         name='ask'),
+    path('private-question/<int:pk>',
+         views.PrivateQuestionDetailView.as_view(),
+         name='private-question'),
+    path('private-question/<int:pk>/create',
+         views.AnswerCreateView.as_view(),
+         name='answer-create')
 ]
