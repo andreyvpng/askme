8db306a3568fe906c0cddd181f5b961c821f5982
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-18 19:38:54 +1000

    added coverage, factory-boy

---
+++ django/core/factories.py
@@ -0,0 +1,29 @@
+import factory
+from user.factories import UserFactory
+
+from .models import Answer, Question
+
+
+class CreateFactoryMixin(factory.DjangoModelFactory):
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        manager = cls._get_manager(model_class)
+        return manager.create(*args, **kwargs)
+
+
+class QuestionFactory(CreateFactoryMixin, factory.DjangoModelFactory):
+    text = factory.Sequence(lambda n: 'Question #%d' % n)
+    asked_to = factory.SubFactory(UserFactory)
+    asked_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Question
+
+
+class AnswerFactory(CreateFactoryMixin, factory.DjangoModelFactory):
+    text = factory.Sequence(lambda n: 'Answer #%d' % n)
+    question = factory.SubFactory(QuestionFactory)
+
+    class Meta:
+        model = Answer
