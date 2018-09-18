51172da5b5890a4d4aae8d010675fff1e142d697
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-18 21:29:48 +1000

    added test_location_label for UserModelTestCase

--- django/user/tests.py
+++ django/user/tests.py
@@ -11,3 +11,6 @@ class UserModelTestCase(TestCase):
 
     def test_get_absolute_url(self):
         self.assertEquals(self.user.get_absolute_url(), '/user/1')
+
+    def test_location_label(self):
+        self.assertEquals(self.user._meta.get_field('location').verbose_name, 'location')
