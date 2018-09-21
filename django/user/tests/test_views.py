70456ced64e85077d9593b27312e799fc355e0c6
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-09-21 19:37:41 +1000

    added UserListViewTest

--- django/user/tests/test_views.py
+++ django/user/tests/test_views.py
@@ -34,3 +34,34 @@ class UserUpdateViewTest(TestCase):
 
         resp = self.client.get('/user/{}/update'.format(self.other_user.id))
         self.assertEqual(resp.status_code, 403)
+
+
+class UserListViewTest(TestCase):
+    USER_LIST_HTML = """
+      <li class="media">
+        <img class='mr-3' src="{image}" alt="">
+        <div class="media-body">
+          <a href="{url}"><h5 class="mt-0 mb-1">{name}</h5></a>
+        </div>
+      </li>
+    """
+
+    def setUp(self):
+        self.user = UserFactory(username='testuser1', password='12345')
+        self.other_user = UserFactory(username='testuser2', password='12345')
+
+    def test_list_of_users_tempalate(self):
+        resp = self.client.get('/user/')
+        rendered_content = resp.rendered_content
+
+        FULL_LIST_OF_USERS = ''
+        users = resp.context_data['object_list']
+
+        for user in users:
+            FULL_LIST_OF_USERS += self.USER_LIST_HTML.format(
+                image=user.avatar_url,
+                url=user.get_absolute_url(),
+                name=user.username
+            )
+
+        self.assertInHTML(FULL_LIST_OF_USERS, rendered_content)
